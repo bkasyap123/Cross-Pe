3cbd61f2a9343d4ea6104f16d3e5af93aec0bed1
Kuldeep yadav <kuldeeep.yadav1@gmail.com>
2024-05-01 17:35:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:2000"],
+    origin: ["http://localhost:5173", "https://easypayz.onrender.com"],
     credentials: true,
   })
 );
