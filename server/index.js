10597127748cccd36108094a014e361baef32580
Kuldeep yadav <kuldeeep.yadav1@gmail.com>
2024-05-01 21:55:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://easypayz.onrender.com", "http://localhost:3000"],
+    origin: ["https://easypayz.onrender.com", "https://easypayz.vercel.app"],
     credentials: true,
   })
 );
