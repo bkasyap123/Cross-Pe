e1307db862c47ba27acbb4a099509b05aa2de63e
Kuldeep yadav <kuldeeep.yadav1@gmail.com>
2024-05-01 17:39:47 +0530

    Update dbConfig.js

--- server/model/dbConfig.js
+++ server/model/dbConfig.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   database: "EasyPayZ",
   password: process.env.DB_PWD,
