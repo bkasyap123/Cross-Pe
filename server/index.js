b1d0996918e0cdd5d672a5034838e26c8011d1be
kuldeeepy <kuldeeep.yadav1@gmail.com>
2024-05-02 11:06:49 +0530

    server

--- server/index.js
+++ server/index.js
@@ -5,31 +5,21 @@ import "dotenv/config";
 import userRoute from "./routes/userRoutes.js";
 const app = express();
 
-<<<<<<< HEAD
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
-=======
-app.use(cors({origin: "https://easypayz.vercel.app", credentials: true}));
->>>>>>> 329eef21772cfa94059f01964e9c875331e0ba8c
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", userRoute);
 
-<<<<<<< HEAD
 app.get("/health", (req, res) => {
   res.status(200).json({ success: "Health is Ok" });
 });
-=======
-app.get('/health', (req, res) => {
-  res.status(200).json({success: 'Health is Ok'})
-})
->>>>>>> 329eef21772cfa94059f01964e9c875331e0ba8c
 
 app.use((error, req, res, next) => {
   let { message } = error;
