4e1942644388d7cf55f1e4c98a36ad4c6e46bff2
Kuldeep yadav <kuldeeep.yadav1@gmail.com>
2024-05-02 20:44:18 +0530

    Update zustAuth.jsx

--- client/src/authStore/zustAuth.jsx
+++ client/src/authStore/zustAuth.jsx
@@ -2,7 +2,10 @@ import { create } from "zustand";
 
 const useAuth = create((set) => ({
   isLoggedIn: !!document.cookie.slice(6),
-  setLoggedIn: (loggedIn) => set({ isLoggedIn: loggedIn }),
+  setLoggedIn: (loggedIn) => {
+    set({ isLoggedIn: loggedIn });
+    localStorage.setItem("isLoggedIn", loggedIn ? "true" : "false");
+  }
 }));
 
 export default useAuth;
