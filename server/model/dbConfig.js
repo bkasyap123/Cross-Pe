0accf518d18d08d0cb005472685e15eb1e90dda1
Kuldeep yadav <kuldeeep.yadav1@gmail.com>
2024-05-06 15:56:05 +0530

    Update dbConfig.js

--- server/model/dbConfig.js
+++ server/model/dbConfig.js
@@ -1,25 +1,3 @@
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const url = process.env.MONGO_URL;
-
-// const client = new MongoClient(url, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     await client.connect();
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Successfully connected to MongoDB!");
-//   } finally {
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 import mongoose from "mongoose";
 
 dbConfig().catch((err) => console.log(err));
